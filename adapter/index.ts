1b27466d9ebaa9d1901a4c9e111651255e6aa82d
Luuk <me@luuk.network>
2025-12-29 01:38:00 +0100

    Add Scaleway adapter with deployment script

---
+++ adapter/index.ts
@@ -0,0 +1,36 @@
+import type { AstroIntegration } from "astro";
+
+export default function createIntegration(): AstroIntegration {
+  return {
+    name: "@slimluccii/astro-scaleway-adapter",
+    hooks: {
+      "astro:config:setup": ({ updateConfig, config }) => {
+        updateConfig({
+          build: {
+            client: new URL(`./static/`, config.outDir),
+            server: new URL(config.outDir),
+            serverEntry: "handler.mjs",
+            redirects: false,
+          }
+        });
+      },
+      "astro:config:done": ({ setAdapter }) => {
+        setAdapter({
+          name: "@slimluccii/astro-scaleway-adapter",
+          serverEntrypoint: "./adapter/handler.ts",
+          exports: ["handle"],
+          supportedAstroFeatures: {
+            staticOutput: "stable",
+            hybridOutput: "stable",
+            serverOutput: "stable",
+            envGetSecret: "stable",
+            sharpImageService: "stable",
+          },
+          adapterFeatures: {
+            edgeMiddleware: false,
+          },
+        });
+      },
+    },
+  };
+}
