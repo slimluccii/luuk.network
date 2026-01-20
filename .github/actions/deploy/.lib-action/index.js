3ed833586f015ac2049ae32d9199e908460085b7
Luuk <mail@luuk.network>
2026-01-20 20:40:37 +0100

    Log upload URL during function upload

--- .github/actions/deploy/.lib-action/index.js
+++ .github/actions/deploy/.lib-action/index.js
@@ -30156,6 +30156,7 @@ const uploadServerlessFunction = async ({ functionId }) => {
         }),
     });
     const { url: uploadUrl, headers: uploadHeaders } = await response.json();
+    console.log(uploadUrl);
     await fetch(uploadUrl, {
         method: "PUT",
         headers: uploadHeaders,
