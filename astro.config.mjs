bec144b0d56939ea42da5eecd3e7d8c0580060ae
Luuk <mail@luuk.network>
2026-04-27 22:44:26 +0200

    fix(security): remove CSP configuration and delete headers file

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,9 +13,6 @@ export default defineConfig({
     imageService: "compile",
   }),
   integrations: [designTokens(), iconSprite(), sitemap()],
-  security: {
-    csp: true,
-  },
   devToolbar: {
     enabled: false,
   },
