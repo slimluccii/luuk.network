b1eafd84f2d15b0382ca9b5b9b2a27a222a577c9
Luuk <mail@luuk.network>
2026-09-08 09:41:26 +0200

    chore: remove the bunny spike deploy tooling

--- router/floor.ts
+++
@@ -1,3 +0,0 @@
-// Scenario 1: measure Bunny's floor with a script that does nothing.
-import * as BunnySDK from "@bunny.net/edgescript-sdk";
-BunnySDK.net.http.servePullZone().onOriginRequest(async () => new Response("ok"));
